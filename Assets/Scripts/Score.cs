da77b676117e58ed8c376aabf6846f7e1d091c75
Niels <Niels97vet@hotmail.com>
2016-05-31 11:30:02 +0200

    aaaaa

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+    public Scrollbar scoreBarr;
+    public float score = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
