0794d6cb290d926944e27ea2cff6d28e063c7b2e
Niels <Niels97vet@hotmail.com>
2016-06-16 10:28:36 +0200

    pvp

---
+++ Assets/Scripts/PvP_Bar.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bar : MonoBehaviour {
+    [SerializeField]
+    private PickUp time;
+    [SerializeField]
+    private GameObject BarPlayer1;
+    [SerializeField]
+    private GameObject BarPlayer2;
+    [SerializeField]
+    private float maxTime = 30f;
+    [SerializeField]
+    private float currentKillsPlayer1 = 0f;
+    [SerializeField]
+    private float currentKillsPlayer2 = 0f;
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+	}
+    public void SetBarPlayer1()
+    {
+
+    }
+    public void SetBarPlayer2()
+    {
+
+    }
+}
